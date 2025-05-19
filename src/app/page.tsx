b3aeb99c5373ceb0528f89fc7f3db0cbc554367f
Brodi White <brodibranded@gmail.com>
2025-05-19 07:03:48 +0000

    move the form down to the bottom just above the header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import HeroSection from "@/components/sections/home/HeroSection";
 import AboutSection from "@/components/sections/home/AboutSection";
 import ServicesSection from "@/components/sections/home/ServicesSection";
 import PointsSystemSection from "@/components/sections/home/PointsSystemSection";
-import GetQuoteBanner from "@/components/sections/home/GetQuoteBanner"; // New
+import GetQuoteBanner from "@/components/sections/home/GetQuoteBanner";
 import PricingSection from "@/components/sections/home/PricingSection";
 import ContactFormSection from "@/components/sections/home/ContactSection";
 import MapContactSection from "@/components/sections/home/MapContactSection";
@@ -20,11 +20,11 @@ export default function HomePage() {
         <AboutSection />
         <ServicesSection />
         <PointsSystemSection />
-        <GetQuoteBanner /> {/* New Banner Added Here */}
+        <GetQuoteBanner />
         <PricingSection />
-        <ContactFormSection />
         <MapContactSection />
         <BlogSection />
+        <ContactFormSection /> {/* Moved to be last content section */}
       </main>
       <Footer />
     </>
